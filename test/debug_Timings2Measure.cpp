b00b3bc62d09714d2219375c7cda7a18efae1e30
Emanuele Iannone <emanuele@fondani.it>
2019-01-13 17:38:40 +0100

    Some code for debugging and testing

--- test/debug_Timings2Measure.cpp
+++ test/debug_Timings2Measure.cpp
@@ -2,13 +2,14 @@
 // Created by Emanuele on 29/06/2018.
 //
 #ifdef DEBUG
+
 #include <iostream>
 #include <cstring>
 #include "Timings2Measure.h"
 
 struct packet : timings_packet {
     long unsigned int timings[200];
-    uint32_t peekTiming(size_t pos) {
+    uint32_t peekTiming(size_t pos) override {
         return (pos >= 0 && pos < size)? (uint32_t) timings[pos] : 0xFFFFFFFF;
     }
 };
@@ -30,6 +31,7 @@ int main( int argc, char **argv) {
 
     freopen("test_Timings2Measure.dat", "r", stdin);
     std::cin >> nTests;
+    int ok = 0;
     printf("N. misure di test: %d\r\n", nTests);
     for(int t = 0; t < nTests; t++) {
         scanf("%lu %d %d.%d %d %s", &msec, &nTimings, &units, &decimals, &sensorAddr, mType);
@@ -39,10 +41,16 @@ int main( int argc, char **argv) {
         for(int tm = 0; tm < pk.size; tm++) scanf("%lu", &pk.timings[tm]);
 
         measure m = t2m.getMeasure(&pk);
+        bool check = (m.sensorAddr == sensorAddr
+                && strcmp(mTypeToStr(m.type), (const char*)mType) == 0
+                && m.units == units
+                && m.decimals == decimals);
+        if (check) ok++;
 
-        printf("%d (%d): %d %s %d.%d\n", m.msec, pk.size, m.sensorAddr,
-               mTypeToStr(m.type), m.units, m.decimals);
+        printf("%d: %d %s %d.%d ", m.msec, m.sensorAddr, mTypeToStr(m.type), m.units, m.decimals);
+        std::cout << (check? "OK" : "NO") << "\n";
     }
+    printf("\n OK: %d/%d\n", ok, nTests);
 }
 
 #endif
